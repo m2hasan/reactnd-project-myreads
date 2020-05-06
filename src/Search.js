bd15e360681b20604361615a883c29ccd7112636
Mahbub <mmhasaneee@gmail.com>
2020-05-06 13:12:32 +0600

    Load search results on typing

--- src/Search.js
+++ src/Search.js
@@ -1,26 +1,44 @@
 import React, {Component} from 'react';
-
+import * as BooksAPI from './BooksAPI';
+import BookList from './BookList';
 class Search extends Component {
+  state = {
+    books: [],
+    query: ''
+  }
+  onQueryChange = (event) => {
+    const query = event.target.value;
+    this.setState((currentState) => ({
+      query
+    }));
+    BooksAPI
+      .search(query)
+      .then((books) => {
+        books && !books.error
+          ? this.setState((currentState) => ({
+              books
+            }))
+          : this.setState((currentState) => ({
+            books: []
+          }));
+      });
+  }
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <button className="close-search" onClick={this.props.onSearchClose}>Close</button>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={this.onQueryChange}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <BookList books={this.state.books} onBookMoveTo={this.props.onBookMoveTo} />
         </div>
       </div>
     );
